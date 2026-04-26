bdc96841b7654990ee859cc8eb2fe125ec3c4acb
Griffy321 <griffinjames435@gmail.com>
2026-04-26 14:09:25 +0100

    save

--- Stock App/app.py
+++ Stock App/app.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
 
-df
