e2abd879bcde975f69a0fa27147fa7a9e0b87b8d
Griffy321 <griffinjames435@gmail.com>
2026-04-26 21:42:59 +0100

    Exploring streamlit

--- Stock App/app.py
+++ Stock App/app.py
@@ -4,4 +4,16 @@ import numpy as np
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
+# Docs - https://docs.streamlit.io/develop/api-reference
+# You can have mpre than one page in streamlit - good for if we want to add some kind of "add to t212 pie" feature 
 
+st.title("Stock Comparison Dashboard")
+ticker = st.text_input("Please Enter a Ticker to Search:").upper()
+st.caption("(one at a time)")
+st.write(f"searched for: {ticker}")
+
+st.subheader("Comparison Table")
+results_table = pd.DataFrame()
+st.write(results_table)
+
+st.subheader("Comparison Chart")
