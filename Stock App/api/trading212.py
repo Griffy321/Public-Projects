ce48dd7b4676d7434b11094fcc407a94bf229081
Griffy321 <griffinjames435@gmail.com>
2026-05-02 10:35:03 +0100

    getting pie data

--- Stock App/api/trading212.py
+++ Stock App/api/trading212.py
@@ -20,38 +20,39 @@ api_key = os.getenv("t212_api_key")
 t212_secret_key = os.getenv("t212_secret_key")
 
 class TradingAccount():
-    def __init__(self, api_key):
-        self.api_key = api_key
+    def __init__(self, api_key, secret_key):
+        credentials = base64.b64encode(f"{api_key}:{secret_key}".encode()).decode()
+        self.auth_header = {"Authorization": f"Basic {credentials}"}
         self.pies = []
 
     def get_account_info(self):
         account_url = "https://live.trading212.com/api/v0/equity/account/summary"
-        headers = {"Authorization": self.api_key}
-        response = requests.get(url=account_url, headers=headers)
+        response = requests.get(url=account_url, headers=self.auth_header)
         if response.status_code == 429:
             time.sleep(6)
-            response = requests.get(url=account_url, headers=headers)
+            response = requests.get(url=account_url, headers=self.auth_header)
         if response.status_code != 200:
             print(f"Request failed. Status: {response.status_code}")
             return {}
         return response.json()
-    
+
     def update_pie_ids(self, pie_data):
         for pie in pie_data:
             pie_id = pie.get("id")
+            data = self.get_one_pie(pie_id=pie_id)
+            pie_name = data.get("settings").get("name")
             if pie_id in self.pies:
                 pass
             else:
-                self.pies.append(pie_id)
+                self.pies.append({pie_id : pie_name})
         print(self.pies)
 
     def get_all_pies(self):
         pie_url = "https://live.trading212.com/api/v0/equity/pies"
-        headers = {"Authorization": self.api_key}
-        response = requests.get(url=pie_url, headers=headers)
+        response = requests.get(url=pie_url, headers=self.auth_header)
         if response.status_code == 429:
             time.sleep(6)
-            response = requests.get(url=pie_url, headers=headers)
+            response = requests.get(url=pie_url, headers=self.auth_header)
         if response.status_code != 200:
             print(f"Request to trading 212 api did not work. Status: {response.status_code}")
             return {}
@@ -60,21 +61,28 @@ class TradingAccount():
     def get_one_pie(self, pie_id=None):
         if pie_id is None:
             self.update_pie_ids(self.get_all_pies())
+            if not self.pies:
+                print("No pies available.")
+                return {}
             index = input("enter the pie you want to view (0 indexed):")
             index = int(index)
-            pie_id = self.pies[index]
+            pie_id = self.pies[index].keys()
+            pie_id = next(iter(pie_id))
         pie_url = f"https://live.trading212.com/api/v0/equity/pies/{pie_id}"
-        headers = {"Authorization": self.api_key}
-        response = requests.get(url=pie_url, headers=headers)
+        response = requests.get(url=pie_url, headers=self.auth_header)
         if response.status_code == 429:
             time.sleep(6)
-            response = requests.get(url=pie_url, headers=headers)
+            response = requests.get(url=pie_url, headers=self.auth_header)
         if response.status_code != 200:
             print(f"Request to trading 212 api did not work. Status: {response.status_code}")
-            return {}
+            return []
         return response.json()
-    
 
-account = TradingAccount(api_key)
+    def update_pie(self, pie_id):
+        update_url = f"https://live.trading212.com/api/v0/equity/pies/{pie_id}"
+        
+        load = {}
+        requests.post()
+
+account = TradingAccount(api_key, t212_secret_key)
 print(account.get_one_pie())
-
